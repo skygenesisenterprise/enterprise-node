a88b88e81a80d8cf7a70766fa70ff2448a241329
liam dispa <liam.dispa@skygenesisenterprise.com>
2025-11-13 17:18:38 +0100

    fix error build package environment

--- packages/core/rollup.config.js
+++ packages/core/rollup.config.js
@@ -1,22 +1,28 @@
 import { defineConfig } from 'rollup';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
+import typescript from '@rollup/plugin-typescript';
 
 export default defineConfig([
   {
-    input: 'dist/index.js',
+    input: 'src/index.ts',
     output: [
       {
-        file: 'dist/index.bundle.js',
+        file: 'dist/index.js',
         format: 'cjs',
         sourcemap: true,
       },
       {
-        file: 'dist/index.esm.bundle.js',
+        file: 'dist/index.esm.js',
         format: 'es',
         sourcemap: true,
       },
     ],
     plugins: [
+      typescript({
+        tsconfig: './tsconfig.json',
+        declaration: true,
+        declarationDir: 'dist',
+      }),
       nodeResolve({
         preferBuiltins: true,
       }),
